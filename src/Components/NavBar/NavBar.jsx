16841b1e901ff1e6e028ed227d34753976cd7e6e
mohiqudraty <mohi.qudraty@gmail.com>
2023-09-25 22:24:15 +0600

    navBar link active color or underlined added

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+
 const NavBar = () => {
   return (
     <nav className="container mx-auto flex flex-col gap-5 md:flex-row justify-between items-center py-10 px-6">
@@ -7,13 +7,40 @@ const NavBar = () => {
         <img className="h-[72px]" src="/src/assets/Logo.png" alt="" />
       </div>
       <ul className="flex items-center gap-10">
-        <NavLink to={"/"}>
+        <NavLink
+          to={"/"}
+          className={({ isActive, isPending }) =>
+            isActive
+              ? "text-lg font-bold underline text-[#FF444A]"
+              : isPending
+              ? "pending"
+              : "text-lg"
+          }
+        >
           <li>Home</li>
         </NavLink>
-        <NavLink to={"/donation"}>
+        <NavLink
+          to={"/donation"}
+          className={({ isActive, isPending }) =>
+            isActive
+              ? "text-lg font-bold underline text-[#FF444A]"
+              : isPending
+              ? "pending"
+              : "text-lg"
+          }
+        >
           <li>Donation</li>
         </NavLink>
-        <NavLink to={"/statistics"}>
+        <NavLink
+          to={"/statistics"}
+          className={({ isActive, isPending }) =>
+            isActive
+              ? "text-lg font-bold underline text-[#FF444A]"
+              : isPending
+              ? "pending"
+              : "text-lg"
+          }
+        >
           <li>Statistics</li>
         </NavLink>
       </ul>
